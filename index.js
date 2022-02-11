a84f3dd1f0c1ebee3275028d8da21a034ae6f783
stenvis <vladustenko@gmail.com>
2022-02-11 16:48:56 +0200

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+function checkSupport() {
+   const canvas = document.getElementById('canvas');
+   if (
+     !window.WebGLRenderingContext &&
+     !canvas.getContext('webgl')
+   ) return; // redirect simplepage;
+   loadApplication();
+};
+
+function loadApplication() {
+   const
+      head = document.head,
+      baseStyles = document.createElement('link'),
+      loader  = document.createElement('script');
+
+   baseStyles.rel = 'stylesheet';
+   baseStyles.href = '/webgl/styles/index.css';
+   baseStyles.defer = true;
+   baseStyles.type = 'text/css';
+   head.appendChild(baseStyles);
+
+   loader.src = '/webgl/loader.js';
+   baseStyles.defer = true;
+   loader.type = 'module';
+   head.appendChild(loader);
+
+   document.removeEventListener('DOMContentLoaded', checkSupport);
+};
+
+{
+   document.addEventListener('DOMContentLoaded', checkSupport);
+}
---
+++ webgl/lib/helpers/domnode.js
@@ -0,0 +1,13 @@
+const GEBCN = name => document.getElementsByClassName(name);
+const QS = selector => document.querySelector(selector);
+const GEBI = id => document.getElementById(id);
+const GEBTN = name => document.getElementsByTagName(name);
+
+const DOMNode = {
+   GEBCN,
+   QS,
+   GEBI,
+   GEBTN,
+};
+
+export default DOMNode;
